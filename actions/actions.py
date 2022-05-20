56c8d708319e8f0e466ab621efeb35d754e0c121
david <a20davidmf@iessanclemente.net>
2022-05-20 16:45:11 +0100

    16:45

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,24 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Text, Any, Dict, List
+
+from random import Random
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+     def name(self) -> Text:
+        return "action_hello_world"
+
+     def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         numero = Random.randint(4)
+
+         dispatcher.utter_message(text="Hello World! I'm using a action. This is the number: " + str(numero))
+         return []
